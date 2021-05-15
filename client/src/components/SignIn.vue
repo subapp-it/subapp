e71303c2657282a68d1fb3901828462d1179cb68
dcascone <dario.cascone93@gmail.com>
2021-05-16 00:43:43 +0200

    deploy

--- client/src/components/SignIn.vue
+++ client/src/components/SignIn.vue
@@ -1075,7 +1075,7 @@ export default {
       this.durcRegolarityFile = new File([''], 'Durc')
     },
     goToPrivacy () {
-      const link = process.env.NODE_ENV === 'development' ? 'localhost:3000/public/info&privacy.pdf' : '/public/info&privacy'
+      const link = process.env.NODE_ENV === 'development' ? 'localhost:3000/public/info&privacy.pdf' : '/public/info&privacy.pdf'
       window.open(link, '_blank')
     },
     goToTeC () {
